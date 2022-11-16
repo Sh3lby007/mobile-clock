1a6e807b130b7ab1ff05cee43a0989ff038742c5
Sh3lby007 <zhengshaobin@ymail.com>
2022-11-16 16:05:00 +0800

    Refactor Navbar item to route correctly.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,28 +8,32 @@ const showMenu = ref(false)
   <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
 
     <!-- Most left side of navbar -->
-  <div class="navbar-brand">
-    <a class="navbar-item" href="https://vuejs.org/">
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="30" height="30" />
-    </a>
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://vuejs.org/">
+        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="30" height="30" />
+      </a>
 
-    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" @click="showMenu = !showMenu" :class="{'is-active': showMenu}">
-      <span></span>
-      <span></span>
-      <span></span>
-    </a>
-  </div>
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" @click="showMenu = !showMenu"
+        :class="{'is-active': showMenu}">
+        <span></span>
+        <span></span>
+        <span></span>
+      </a>
+    </div>
 
-<!-- Most right side of navbar -->
-<div class="navbar-menu is-active has-text-right" :class="{'is-active': showMenu}">
-  <div class="navbar-end">
-    <a class="navbar-item">Clock</a>
-    <a class="navbar-item">Settings</a>
-    <a class="navbar-item" href="https://github.com/Sh3lby007/mobile-clock">Github</a>
-  </div>
-</div>
+    <!-- Most right side of navbar -->
+    <div class="navbar-menu is-active has-text-right" :class="{ 'is-active': showMenu }">
+      <div @click="showMenu = false" class="navbar-end">
 
-</nav>
+        <router-link to="/" class="navbar-item">Clock</router-link>
+
+        <router-link to="/Settings" class="navbar-item">Settings</router-link>
+
+        <a class="navbar-item" href="https://github.com/Sh3lby007/mobile-clock">Github</a>
+      </div>
+    </div>
+
+  </nav>
 
 </template>
 
@@ -50,6 +54,7 @@ h3 {
 }
 
 @media (min-width: 1024px) {
+
   .greetings h1,
   .greetings h3 {
     text-align: left;
