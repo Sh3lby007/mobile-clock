3d379cf36fb865f5eaaaa54448e8b645446c86e6
Sh3lby007 <zhengshaobin@ymail.com>
2022-11-17 14:44:49 +0800

    Refactor dev script and add time.     Change port to 8080 for npm run dev script.     Include Date, time and timezone in a box in the home view, with it     refreshing every 1 second.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,29 @@
-<!-- What do i need?
-1. A box/container which display the time
-2. A div that shows the timezone
-3. Another section that shows the day, date and year -->
-
+<!-- 
+1. Get the time now from JavaScript
+2. Get the time and display it into a box
+3. Show the box to display it in
+4. Make the time change every second  -->
 
 <script setup>
+import { ref } from "vue";
 
+let time = ref(new Date());
+
+setInterval(function () {
+  time.value = new Date();
+}, 1000);
+
+// in settings page, add button to click back to home page
+// in settings page, add a select dropdown for user to choose type of date/time display they want
+//     for the above selection, save the data into pinia store
 </script>
 
 <template>
-  <div class="container is-fluid">
-
-    <div class="timezone">
-      GTMT + 8
-      <div class="day-date">
-        TEST DAY, TEST DATE, 2022
-        <div class="box">
-
-        </div>
-
-
+  <div class="columns is-mobile is-centered">
+    <div class="column is-half">
+      <div class="box">
+        {{ time }}
       </div>
-
     </div>
   </div>
 </template>
-
-<style>
-
-</style>
