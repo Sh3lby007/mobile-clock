2fdc18763e5659910adc5680879dd0588476973c
Sh3lby007 <zhengshaobin@ymail.com>
2022-11-18 15:13:13 +0800

    Basic Layout of settings page.

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,15 +1,60 @@
 <template>
-  <div class="Settings">
-    <h1>Settings Page</h1>
+
+  <div class="container is-fullhd">
+    <div class="section py-o">
+      <div class="columns is-mobile is-centered is-multiline is-desktop">
+        <div class="column is-full">
+          <div class="box">
+            Version
+          </div>
+        </div>
+
+        <div class="column is-full">
+          <div class="box">
+            Stay Awake
+          </div>
+        </div>
+
+        <div class="column is-full">
+          <div class="box">
+            High Frequency
+          </div>
+        </div>
+
+        <div class="column is-full">
+          <div class="box">
+            Choose Time format
+            <div class="select is-primary is-fullwidth">
+              <select>
+                <option>Time Only</option>
+                <option>Short Date and Time</option>
+                <option>Date and Time</option>
+              </select>
+            </div>
+          </div>
+        </div>
+
+        <div class="column is-full">
+          <div class="box">
+            Check against network clock
+          </div>
+        </div>
+        <div class="column is-full">
+
+          <button @click="$router.push('/')" class="button is-primary is-large is-fullwidth is-responsive">Home</button>
+        </div>
+      </div>
+    </div>
   </div>
+
 </template>
 
+
+<script setup>
+
+</script>
+
+
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+
 </style>
