dadc162392a52df6a48f3c4904c12198423f7aa1
Sh3lby007 <zhengshaobin@ymail.com>
2022-11-19 20:46:18 +0800

    Initial Commit for timer.

--- src/views/Timer.vue
+++ src/views/Timer.vue
@@ -1,3 +1,25 @@
-<template>
+<script setup>
+import { ref } from 'vue'
 
+let timeLeft = ref(60)
+
+const countDown = setInterval(timer, 1000)
+
+function timer() {
+    if (timeLeft > 0) {
+        timeLeft.value--
+    } else {
+        clearInterval(countDown)
+    }
+}
+
+
+</script>
+
+    
+<template>
+    <div class="timer">
+        {{ timeLeft }}
+    </div>
+    <button @click="countDown" class="button is-primary is-fullwidth is-responsive">Start</button>
 </template>
