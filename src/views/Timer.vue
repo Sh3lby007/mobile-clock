acb506af174a604ab5918ff255de89499f98a0a9
Sh3lby007 <zhengshaobin@ymail.com>
2022-11-21 18:31:57 +0800

    Timer styling and fix setInterval issue:     Timer runs when page is load instead of when button is clicked in the previous version, fix that by seperating the setInterval and clearInterval into two seperate functions.     Timer countDown function gets called repeatedly previously but is fix by adding @click.once on the event handler to only listen to it once.     Simple styling in prepping hours, minutes and seconds selection for future timer.

--- src/views/Timer.vue
+++ src/views/Timer.vue
@@ -1,25 +1,39 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
-let timeLeft = ref(60)
+let timeLeft = ref(60);
 
-const countDown = setInterval(timer, 1000)
-
-function timer() {
-    if (timeLeft > 0) {
-        timeLeft.value--
-    } else {
-        clearInterval(countDown)
-    }
+function interval() {
+  if (timeLeft.value > 0) {
+    timeLeft.value--;
+  } else {
+    clearInterval(interval);
+  }
 }
 
-
+function countDown() {
+  setInterval(interval, 1000);
+}
 </script>
 
-    
 <template>
-    <div class="timer">
-        {{ timeLeft }}
+  <div class="columns is-mobile is-multiline is-centered">
+    <div class="column is-full">
+      <h1 class="has-text-centered">Timer</h1>
+      <h1 class="has-text-centered">{{ timeLeft }}</h1>
     </div>
-    <button @click="countDown" class="button is-primary is-fullwidth is-responsive">Start</button>
+
+    <div class="column">Hours box</div>
+    <div class="column">Minutes box</div>
+    <div class="column">Seconds box</div>
+
+    <div class="column is-full">
+      <button
+        @click.once="countDown"
+        class="button is-primary is-fullwidth is-responsive"
+      >
+        Start
+      </button>
+    </div>
+  </div>
 </template>
