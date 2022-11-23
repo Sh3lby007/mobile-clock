2d8664993039de971eae607f2d1e7e6ccf0c2800
Sh3lby007 <zhengshaobin@ymail.com>
2022-11-23 17:02:22 +0800

    Fix Navbar toggle functionality.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,51 +5,35 @@ const showMenu = ref(false);
 </script>
 
 <template>
-  <nav
-    class="navbar is-fixed-top"
-    role="navigation"
-    aria-label="main navigation"
-  >
-    <!-- Most left side of navbar -->
-    <div class="navbar-brand">
-      <router-link to="/" class="navbar-item">
-        <img
-          alt="Vue logo"
-          class="logo"
-          src="@/assets/logo.svg"
-          width="30"
-          height="30"
-        />
-      </router-link>
-
-      <a
-        role="button"
-        class="navbar-burger"
-        aria-label="menu"
-        aria-expanded="false"
-        @click="showMenu = !showMenu"
-        :class="{ 'is-active': showMenu }"
-      >
-        <span></span>
-        <span></span>
-        <span></span>
-      </a>
-    </div>
-
-    <!-- Most right side of navbar -->
-    <div
-      class="navbar-menu is-active has-text-right"
-      :class="{ 'is-active': showMenu }"
-    >
-      <div @click="showMenu = false" class="navbar-end">
-        <router-link to="/" class="navbar-item">Clock</router-link>
-        <router-link to="/Timer" class="navbar-item">Timer </router-link>
-        <router-link to="/Settings" class="navbar-item">Settings</router-link>
-
-        <a class="navbar-item" href="https://github.com/Sh3lby007/mobile-clock"
-          >Github</a
-        >
+  <!-- Hero header: will stick at the top -->
+  <div class="hero-head">
+    <nav class="navbar">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item">
+          <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="30" height="30" />
+        </router-link>
+        <!--
+    Using the v-on: directive to listen for the click event and toggle the data property showNav. Also, using the v-bind: directive to reactively update the class attribute 'is-active' based on the showNav property.
+    -->
+        <div class="navbar-burger" @click="showMenu = !showMenu" :class="{ 'is-active': showMenu }">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
-    </div>
-  </nav>
+      <!--
+    Using the v-bind: directive to reactively update the class attribute 'is-active' based on the showNav property.
+    -->
+      <div class="navbar-menu has-text-right" :class="{ 'is-active': showMenu }">
+        <div class="navbar-end">
+
+          <router-link to="/" class="navbar-item">Clock</router-link>
+          <router-link to="/Timer" class="navbar-item">Timer </router-link>
+          <router-link to="/Settings" class="navbar-item">Settings</router-link>
+          <a class="navbar-item" href="https://github.com/Sh3lby007/mobile-clock">Github</a>
+
+        </div>
+      </div>
+    </nav>
+  </div>
 </template>
